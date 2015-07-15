968752486ddfb88ac9fc553761b7ce02be57e7b7
John Olmsted <john.olmsted@creditkarma.com>
2015-07-15 12:51:57 -0700

    Set up basic RESTful routes for /users

--- models/user.js
+++ models/user.js
@@ -1,9 +1,8 @@
-"use strict";
+'use strict';
 
-module.exports = function(sequelize, Datatypes) {
-  var User = sequelize.define("User", {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define('User', {
+    username: DataTypes.STRING
   });
 
   return User;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,68 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
-/* GET /users */
+/* GET /api/users */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  models.User.findAll()
+    .then(function(users) {
+      res.status = 200;
+      res.json({
+        status: res.status,
+        message: 'OK',
+        users: users
+      });
+    });
+});
+
+/* GET /api/users/:id */
+router.get('/:id', function(req, res, next) {
+  models.User.findById(req.params.id)
+    .then(function(user) {
+      res.status = 200;
+      res.json({
+        status: res.status,
+        message: 'OK',
+        user: user
+      })
+    });
+});
+
+/* POST /api/users */
+router.post('/', function(req, res, next) {
+  models.User.create(req.body)
+    .then(function(user) {
+      res.status = 201;
+      res.json({
+        status: res.status,
+        message: 'User created',
+        user: user
+      });
+    });
+});
+
+/* PUT /api/users/:id */
+router.put('/:id', function(req, res, next) {
+  models.User.update(req.body, { where: { id: req.params.id } })
+    .then(function() {
+      res.status = 200;
+      res.json({
+        status: res.status,
+        message: 'User updated'
+      });
+    });
+});
+
+/* DELETE /api/users/:id */
+router.delete('/:id', function(req, res, next) {
+  models.User.destroy({ where: { id: req.params.id } })
+    .then(function() {
+      res.status = 200;
+      res.json({
+        status: res.status,
+        message: 'User deleted'
+      });
+    });
 });
 
 module.exports = router;
