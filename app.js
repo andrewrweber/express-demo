22ed6a3495dbcd472c55ec272a100f28af2fc8b3
John Olmsted <john.olmsted@creditkarma.com>
2015-07-15 10:41:08 -0700

    Set up users model

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var users = require('./routes/users');
 
 var app = express();
 
+var router = express.Router();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -22,8 +24,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/api', routes);
+app.use('/api/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,7 +41,8 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.json({
+      status: res.status,
       message: err.message,
       error: err
     });
@@ -50,11 +53,11 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.json({
+    status: res.status,
     message: err.message,
     error: {}
   });
 });
-
 
 module.exports = app;
